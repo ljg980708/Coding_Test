c66b991f8724e32c7d7c6b54795b0382eb58fbb9
ljg980708 <ljg980708@gmail.com>
2023-02-23 22:46:49 +0900

    23-02-23

---
+++ programmers/curse_num_3(120871)/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    static class Solution {
+        public int solution(int n) {
+            int answer = 1;
+            int i = 1;
+            while(true){
+                if(answer % 3 == 0 || answer % 10 == 3 || answer / 10 == 3 || answer / 10 == 13){
+                    // answer%3==0 || answer%10==3 || answer/10==3 || answer/10==13
+                    answer += 1;
+                }else{
+                    if(i == n) break;
+                    answer +=1;
+                    i++;
+                }
+            }
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(s.solution(num));
+    }
+}
